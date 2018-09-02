48a5711d4f88b6d62e84ea96464d1bbaf97f6bfd
lijian <905493036@qq.com>
2018-09-02 22:16:01 +0800

    跑通测试模块

---
+++ cms-tester/src/test/java/com/codersoft/cms/tester/TestA.java
@@ -0,0 +1,16 @@
+package com.codersoft.cms.tester;
+
+import org.junit.Test;
+
+/**
+ * Created by wow-lj on 2018/9/2.
+ */
+public class TestA extends BasicTest {
+
+
+    @Test
+    public void test(){
+        System.out.println("hhhhhhhhhhhhhhh");
+    }
+
+}
